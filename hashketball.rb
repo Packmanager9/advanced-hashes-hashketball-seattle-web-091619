2aa8ceb935d4ee8cd44b8ed39a68375f279c23b1
Cole Ditzler <colejditzler@gmail.com>
2019-09-15 23:55:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,7 @@
 # Write your code here!
 
 def game_hash
-hashketxball = {
-
-
-  :home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [{}],
-
-
-    :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => [{}]
+hashketxball = {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [{}]}, :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => [{}]
 
 
 
