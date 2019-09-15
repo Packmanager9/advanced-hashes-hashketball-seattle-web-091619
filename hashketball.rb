e822c07fd9033ddc74c1d813b1280b2b6a188076
Cole Ditzler <colejditzler@gmail.com>
2019-09-15 23:51:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,33 @@
 # Write your code here!
 
+def game_hash do
+hashketball = { 
+
+
+  :home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => {
+
+
+
+
+    
+  },
+  
+  
+    :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => {
+  
+  
+  
+  
+      
+    },
+
+  
+  }  
+}
+
+  
+end
+
 
 
 
