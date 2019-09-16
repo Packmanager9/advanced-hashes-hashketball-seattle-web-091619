85b625ce3588570eebf89f1eba6caba269956dd4
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 00:35:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,7 +114,7 @@ end
 
 
 def num_points_scored(name)
-  hash == {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [{"Alan Anderson" => {
+  hash = {:home => {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [{"Alan Anderson" => {
             :number => 0,
             :shoe => 16,
             :points => 22,
