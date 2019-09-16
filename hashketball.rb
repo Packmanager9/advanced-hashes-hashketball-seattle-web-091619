bf7155cdce7fea97ccc57bf3fc6aa7a41944456b
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 00:33:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,12 +113,12 @@ end
 
 def num_points_scored(name)
 
-  
+
 def num_points_scored(name)
   hash = game_hash
-  hash.each do |location, info| 
-    info.each do |attribute, stuff| 
-      if stuff.include?(name) 
+  hash.each do |location, info|
+    info.each do |attribute, stuff|
+      if stuff.include?(name)
        return hash[location][attribute][name][:points]
       end
     end
