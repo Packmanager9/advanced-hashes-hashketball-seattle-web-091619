9091f4b3094155cfa2d04535da7a630caeb826d8
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 00:43:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,7 @@ def big_shoe_rebounds
               end
             end
           end
-          puts hash[location][attribute]
+          puts hash[location][attribute][player_name]
         return hash[location][attribute][player_name][:rebounds]
       end
     end
